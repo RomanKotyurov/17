63319cf41a27cac100535c712f24061eecfb12ba
Roman K <rn1nhf@gmail.com>
2024-05-10 20:50:44 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Exchange from "./components/Exchange";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+    // const[headerText, setHeaderText] = useState('Конвертор валют:');
+    const[inputText, setInputText] = useState(1)
+    // const[USD, setUSD] = useState(0)
+    
+    return(
+        <>
+            <h1 id='header'>Конвертор валют</h1>
+            <>RUB </>
+            <input onChange={(event) => {
+                setInputText(event.target.value)
+                // let usd = event.target.value / 100
+                // setUSD(event.target.value / 100)
+            }} value={inputText} />
+
+            {/* <>USD {USD}</> */}
+
+            <Exchange rub = {inputText}/>
+        </>
+    )
 }
-
-export default App;
---
+++ src/components/Exchange.js
@@ -0,0 +1,24 @@
+export default function Exchange(props) {
+
+    let exchangeRate = [
+        {currency: 'USD (доллар США)',
+        rate: 91.82},
+        {currency: 'EUR (евро)',
+        rate: 98.95},
+        {currency: 'CNY (китаййский юань)',
+        rate: 12.62},
+        {currency: 'TRY (турецкая лира)',
+        rate: 2.85}
+    ]
+
+    return(
+        <div className="contents">
+            <h2>Обменный курс:</h2>
+            <ul>
+                {exchangeRate.map(
+                    (exchangeRate, key) => <li key={key}>{exchangeRate.currency}  {(Math.round(props.rub*100 / exchangeRate.rate))/100}</li>
+                )}
+            </ul>
+        </div>
+    )
+}
